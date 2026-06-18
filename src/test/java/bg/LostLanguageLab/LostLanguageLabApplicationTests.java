195db1c7e67fc2902bd3bb22681d904e417dbfe0
MegLaz <paralelepipec@gmail.com>
2026-06-18 20:09:06 +0300

    feat: initialize project

---
+++ src/test/java/bg/LostLanguageLab/LostLanguageLabApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.LostLanguageLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostLanguageLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
