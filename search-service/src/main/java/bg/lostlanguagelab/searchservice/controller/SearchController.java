96f62fce6bd7018f450f27b901380f4954b9761c
MegLaz <paralelepipec@gmail.com>
2026-08-08 23:20:33 +0300

    feat: add SearchController with endpoints

---
+++ search-service/src/main/java/bg/lostlanguagelab/searchservice/controller/SearchController.java
@@ -0,0 +1,27 @@
+package bg.lostlanguagelab.searchservice.controller;
+
+import bg.lostlanguagelab.searchservice.entity.SearchRecord;
+import bg.lostlanguagelab.searchservice.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @PostMapping
+    public SearchRecord saveWord(@RequestParam String word) {
+        return searchService.saveWord(word);
+    }
+
+    @GetMapping
+    public List<SearchRecord> getAllWords() {
+        return searchService.getAllWords();
+    }
+}
+
