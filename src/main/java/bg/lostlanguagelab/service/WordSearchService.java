840f223a182d4036ba14783948f501622653cfec
MegLaz <paralelepipec@gmail.com>
2026-08-08 23:35:02 +0300

    feat: add WordSearchService using Feign client

---
+++ src/main/java/bg/lostlanguagelab/service/WordSearchService.java
@@ -0,0 +1,24 @@
+package bg.lostlanguagelab.service;
+
+import bg.lostlanguagelab.client.SearchServiceClient;
+import bg.lostlanguagelab.model.dto.SearchRecordDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WordSearchService {
+
+    private final SearchServiceClient searchClient;
+
+    public void saveWord(String word) {
+        searchClient.saveWord(word);
+    }
+
+    public List<SearchRecordDto> getAllWords() {
+        return searchClient.getAllWords();
+    }
+}
+
